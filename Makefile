b7eb2c00ce9cd0efca66f53b2c792416375a7fe3
Matt Savino <MSavino@DIRECTV.com>
2014-12-29 14:43:38 -0800

    Testing WIP

---
+++ Makefile
@@ -0,0 +1,5 @@
+test:
+	@./node_modules/.bin/mocha \
+		test/nodule.nu.js
+
+.PHONY: test 
--- test/nodule.nu.js
+++ test/nodule.nu.js
@@ -1,14 +1,22 @@
 var assert = require('assert');
+var sinon = require('sinon');
 var nodulejs = require('../nodule.js');
 
 var app = {};
 var appConfig = {};
 
-
 describe('app', function(){
+  var nodule = nodulejs(app, appConfig);
+
   it('should initialize', function(done){
-    var nodule = nodulejs(app, appConfig);
     assert(typeof nodule === 'object');
     done();
-  })
+  });
+
+  it('should find .js files but exclude .nu.js files', function(done){
+    var spy = sinon.spy();
+    var proxy = once(spy);
+ //    nodule.loadModules(null, 'nu.js');
+    done();
+  });
 });
